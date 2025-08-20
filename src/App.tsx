61569aff039d2d340af45db3dc2d6c16a55b8a1b
Sori <chan8919@naver.com>
2025-08-20 12:21:57 +0900

    <feat> 임시 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import StellarSystem from "./pages/StellarSystem"
 
 function App() {
 
   return (
     <>
       <h1>SONA</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StellarSystem />} />
+          <Route path="/stellar-system" element={<StellarSystem />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
