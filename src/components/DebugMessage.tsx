c4c1ddc2d6a8a0356fd4de63652e3254086576b7
Sori <chan8919@naver.com>
2025-08-29 15:26:24 +0900

    [refactor] stellarSystem 컴포넌트에 id값 추가

--- src/components/DebugMessage.tsx
+++ src/components/DebugMessage.tsx
@@ -1,19 +1,29 @@
-import { useSceneStore } from "@/stores/useSceneStore";
+import { useSceneStore } from '@/stores/useSceneStore';
 
 export default function DebugMessage() {
-    const { focusedPosition,viewMode,cameraIsMoving,cameraTarget } = useSceneStore();
-    
+  const {
+    focusedPosition,
+    viewMode,
+    cameraIsMoving,
+    cameraTarget,
+    selectedStellarSystemId,
+  } = useSceneStore();
 
-    
-
-    return (
-        <div>
-            <div>
-                <p>View Mode: {viewMode}</p>
-                <p>Target Position: {cameraTarget?.x}, {cameraTarget?.y}, {cameraTarget?.z}</p>
-                <p>Focused Position: {focusedPosition?.x}, {focusedPosition?.y}, {focusedPosition?.z}</p>
-                <p>Camera Is Moving: {cameraIsMoving ? 'true' : 'false'}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <p>View Mode: {viewMode}</p>
+        <p>
+          Target Position: {cameraTarget?.x}, {cameraTarget?.y},{' '}
+          {cameraTarget?.z}
+        </p>
+        <p>
+          Focused Position: {focusedPosition?.x}, {focusedPosition?.y},{' '}
+          {focusedPosition?.z}
+        </p>
+        <p>Selected Stellar System ID: {selectedStellarSystemId}</p>
+        <p>Camera Is Moving: {cameraIsMoving ? 'true' : 'false'}</p>
+      </div>
+    </div>
+  );
 }
