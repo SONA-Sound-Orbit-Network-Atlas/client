faa91e18859aafcba0e945493fc290b44299aa8a
Kang Ye Ram <kyj5730@gmail.com>
2025-09-01 15:09:11 +0900

    feat: 컴포넌트 테스트 페이지

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '@/pages/main/Main';
 import GalaxyPage from '@/pages/GalaxyPage';
+import ComponentTestPage from '@/pages/ComponentTestPage';
 import NotFound from '@/pages/NotFound';
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: '/galaxy',
     element: <GalaxyPage />,
+  },
+  {
+    path: '/componentstest',
+    element: <ComponentTestPage />,
   },
   {
     path: '*',
