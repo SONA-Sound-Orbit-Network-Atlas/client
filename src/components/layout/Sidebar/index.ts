1768b724a930d01c57a231c44debc2e5a01e6c2d
Kang Ye Ram <kyj5730@gmail.com>
2025-09-04 15:57:57 +0900

    fix: 내부, 외부 사이드바를 따로 import 할수 없도록 수정정

--- src/components/layout/Sidebar/index.ts
+++ src/components/layout/Sidebar/index.ts
@@ -1,4 +1,2 @@
 // 메인 사이드바 컴포넌트들을 외부로 내보내기
 export { default as Sidebar } from './Sidebar';
-export { default as PrimarySidebar } from './PrimarySidebar';
-export { default as SecondarySidebar } from './SecondarySidebar';
