66ce748447054c21f21d825c46630e8fe7c83f3f
Kang Ye Ram <kyj5730@gmail.com>
2025-09-05 16:01:05 +0900

    feat: 패널의 컴포넌트가 화면의 길이를 넘길경우 스크롤바를 구현현

--- src/components/layout/Sidebar/SecondarySidebar/SecondarySidebar.tsx
+++ src/components/layout/Sidebar/SecondarySidebar/SecondarySidebar.tsx
@@ -2,6 +2,7 @@ import { mergeClassNames } from '@/utils/mergeClassNames';
 import { useSidebarStore } from '@/stores/sidebarStore';
 import MenuContent from './MenuContent';
 import { menuContents } from './constants';
+import { ScrollArea } from '@/components/common/scroll-area';
 
 export default function SecondarySidebar() {
   const { isSecondaryOpen, selectedMenu } = useSidebarStore();
@@ -15,11 +16,15 @@ export default function SecondarySidebar() {
     <div
       className={mergeClassNames(
         'w-64 bg-gray-surface border-r border-gray-border',
-        'flex flex-col gap-4'
+        'flex flex-col h-full'
       )}
     >
       {/* 메뉴 내용 */}
-      <MenuContent content={content} menuId={selectedMenu} />
+      <ScrollArea className="flex-1 h-0">
+        <div>
+          <MenuContent content={content} menuId={selectedMenu} />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
