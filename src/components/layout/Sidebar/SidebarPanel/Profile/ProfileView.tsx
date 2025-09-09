e4110280e8eda1a4ce8d8799892b5a140358cb35
Kang Ye Ram <kyj5730@gmail.com>
2025-09-10 01:14:27 +0900

    feat: 프로필 뷰에 스크롤바 추가

--- src/components/layout/Sidebar/SidebarPanel/Profile/ProfileView.tsx
+++ src/components/layout/Sidebar/SidebarPanel/Profile/ProfileView.tsx
@@ -11,6 +11,7 @@ import { useSidebarStore } from '@/stores/sidebarStore';
 import Iconframe from '@/components/common/Iconframe';
 import Button from '@/components/common/Button';
 import Card from '@/components/common/Card';
+import { ScrollArea } from '@/components/common/Scrollarea';
 
 export default function ProfileView() {
   const { setProfilePanelMode } = useSidebarStore();
@@ -36,89 +37,100 @@ export default function ProfileView() {
   };
 
   return (
-    <div className="flex flex-col h-full overflow-hidden">
-      <div className="flex flex-col items-center border-b border-gray-border p-6">
-        <div className="flex flex-col items-center mb-[24px]">
-          <Iconframe color="tertiary" size="large" className="mb-[16px]">
-            <FiUser />
-          </Iconframe>
-          <h3 className="text-white font-semibold text-base">
-            {profile.username}
-          </h3>
-          <p className="text-text-muted text-sm text-center">{profile.about}</p>
-        </div>
-        <Button color="secondary" size="lg" className="w-full">
-          <FiEdit3 />
-          EDIT PROFILE
-        </Button>
-      </div>
-      <div className="flex flex-col p-6 flex-1">
-        <p className="text-tertiary-200 text-sm font-semibold mb-[16px]">
-          STATISTICS
-        </p>
-        <div className="mb-[24px]">
-          <p className="text-text-muted text-sm mb-[16px]">STELLAR SYSTEMS</p>
-          <Card>
-            <div className="flex items-center justify-center gap-2 mb-[16px]">
-              <IoPlanetOutline className="text-tertiary-300 text-lg" />
-              <p className="text-text-muted text-sm">CREATED</p>
+    <div className="h-full overflow-hidden">
+      <ScrollArea className="h-full">
+        <div className="flex flex-col">
+          <div className="flex flex-col items-center border-b border-gray-border p-6">
+            <div className="flex flex-col items-center mb-[24px]">
+              <Iconframe color="tertiary" size="large" className="mb-[16px]">
+                <FiUser />
+              </Iconframe>
+              <h3 className="text-white font-semibold text-base">
+                {profile.username}
+              </h3>
+              <p className="text-text-muted text-sm text-center">
+                {profile.about}
+              </p>
             </div>
-            <p className="text-white text-center text-[24px] font-semibold">
-              3
+            <Button color="secondary" size="lg" className="w-full">
+              <FiEdit3 />
+              EDIT PROFILE
+            </Button>
+          </div>
+          <div className="flex flex-col p-6">
+            <p className="text-tertiary-200 text-sm font-semibold mb-[16px]">
+              STATISTICS
             </p>
-          </Card>
-        </div>
-        <div className="mb-[24px]">
-          <p className="text-text-muted text-sm mb-[16px]">LIKES</p>
-          <Card onClick={handleLikesClick} className="cursor-pointer">
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <FiHeart className="text-white text-lg mr-4" />
-                <div className="flex flex-col">
-                  <p className="text-white text-lg font-semibold">85</p>
-                  <p className="text-text-muted text-sm">MY LIKES</p>
+            <div className="mb-[24px]">
+              <p className="text-text-muted text-sm mb-[16px]">
+                STELLAR SYSTEMS
+              </p>
+              <Card>
+                <div className="flex items-center justify-center gap-2 mb-[16px]">
+                  <IoPlanetOutline className="text-tertiary-300 text-lg" />
+                  <p className="text-text-muted text-sm">CREATED</p>
                 </div>
-              </div>
-              <FiChevronRight className="text-text-muted text-lg" />
+                <p className="text-white text-center text-[24px] font-semibold">
+                  3
+                </p>
+              </Card>
             </div>
-          </Card>
-        </div>
-        <div className="mb-[24px]">
-          <p className="text-text-muted text-sm mb-[16px]">SOCIALS</p>
-          <div className="flex flex-col gap-[12px] w-full">
-            <Card onClick={handleFollowersClick} className="cursor-pointer">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <FiUser className="text-primary-300 text-lg mr-4" />
-                  <div className="flex flex-col">
-                    <p className="text-white text-lg font-semibold">24</p>
-                    <p className="text-text-muted text-sm">FOLLOWERS</p>
+            <div className="mb-[24px]">
+              <p className="text-text-muted text-sm mb-[16px]">LIKES</p>
+              <Card onClick={handleLikesClick} className="cursor-pointer">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <FiHeart className="text-white text-lg mr-4" />
+                    <div className="flex flex-col">
+                      <p className="text-white text-lg font-semibold">85</p>
+                      <p className="text-text-muted text-sm">MY LIKES</p>
+                    </div>
                   </div>
+                  <FiChevronRight className="text-text-muted text-lg" />
                 </div>
-                <FiChevronRight className="text-text-muted text-lg" />
-              </div>
-            </Card>
-            <Card onClick={handleFollowingsClick} className="cursor-pointer">
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <FiUserCheck className="text-secondary-300 text-lg mr-4" />
-                  <div className="flex flex-col">
-                    <p className="text-white text-lg font-semibold">18</p>
-                    <p className="text-text-muted text-sm">FOLLOWINGS</p>
+              </Card>
+            </div>
+            <div className="mb-[24px]">
+              <p className="text-text-muted text-sm mb-[16px]">SOCIALS</p>
+              <div className="flex flex-col gap-[12px] w-full">
+                <Card onClick={handleFollowersClick} className="cursor-pointer">
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <FiUser className="text-primary-300 text-lg mr-4" />
+                      <div className="flex flex-col">
+                        <p className="text-white text-lg font-semibold">24</p>
+                        <p className="text-text-muted text-sm">FOLLOWERS</p>
+                      </div>
+                    </div>
+                    <FiChevronRight className="text-text-muted text-lg" />
                   </div>
-                </div>
-                <FiChevronRight className="text-text-muted text-lg" />
+                </Card>
+                <Card
+                  onClick={handleFollowingsClick}
+                  className="cursor-pointer"
+                >
+                  <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                      <FiUserCheck className="text-secondary-300 text-lg mr-4" />
+                      <div className="flex flex-col">
+                        <p className="text-white text-lg font-semibold">18</p>
+                        <p className="text-text-muted text-sm">FOLLOWINGS</p>
+                      </div>
+                    </div>
+                    <FiChevronRight className="text-text-muted text-lg" />
+                  </div>
+                </Card>
               </div>
-            </Card>
+            </div>
+          </div>
+          <div className="p-6 border-t border-gray-border">
+            <Button color="tertiary" size="lg" className="w-full">
+              <FiLogOut />
+              SIGN OUT
+            </Button>
           </div>
         </div>
-      </div>
-      <div className="p-6 border-t border-gray-border">
-        <Button color="tertiary" size="lg" className="w-full">
-          <FiLogOut />
-          SIGN OUT
-        </Button>
-      </div>
+      </ScrollArea>
     </div>
   );
 }
