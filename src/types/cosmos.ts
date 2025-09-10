c0b744cae396659ba531e2571dd1d688bb046799
Sori <chan8919@naver.com>
2025-09-10 22:16:41 +0900

    feat: 항성 파라미터 값 표기

--- src/types/cosmos.ts
+++ src/types/cosmos.ts
@@ -1,13 +1,13 @@
 /**
   @property planetSize 행성의 크기, 0.01 ~ 1.00 범위 최적
   @property planetColor 행성의 색상
-  @property planetBrightness 행성의 밝기, 0 ~ 10 ? 자체발광? 이럼 항성인데?
-  @property distanceFromStar 1.0 ~ 10.0 범위 최적 행성과 항성계의 거리,  // 동일 거리 허용할건가?
-  @property orbitSpeed 행성의 공전 속도
-  @property rotationSpeed 행성의 자전 속도
+  @property planetBrightness 행성의 밝기, 0.3 ~ 5.0 ? 자체발광? 이럼 항성인데?
+  @property distanceFromStar 1.0 ~ 20.0 범위 최적 행성과 항성계의 거리,  // 동일 거리 허용할건가?
+  @property orbitSpeed  0.01~1.0 행성의 공전 속도
+  @property rotationSpeed 0.01~1.0 행성의 자전 속도
   @property inclination -180 ~ 180 행성의 기울기
   @property eccentricity 0.0 ~ 0.9 행성의 이심률
-  @property tilt 행성의 기울기
+  @property tilt 0.0 ~ 180.0 행성의 기울기
  */
 
 export type TPlanet = {
