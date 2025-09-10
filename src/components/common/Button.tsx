4b704d9350524d003465027e51a569e69beb5d6c
Kang Ye Ram <kyj5730@gmail.com>
2025-09-11 00:27:31 +0900

    feat: 버튼 컴포넌트 hover시 효과 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,10 +10,11 @@ const buttonVariants = cva(
     variants: {
       color: {
         primary:
-          'bg-gradient-to-r from-secondary-300 to-primary-300 text-text-white border-secondary-300-20',
+          'bg-gradient-to-r from-secondary-300 to-primary-300 text-text-white border-secondary-300-20 hover:brightness-110 hover:border-secondary-300',
         secondary:
-          'bg-tertiary-200-10 text-tertiary-200 border-tertiary-200-20',
-        tertiary: 'text-text-white border-gray-border bg-gray-card',
+          'bg-tertiary-200-10 text-tertiary-200 border-tertiary-200-20 hover:bg-tertiary-200-20 hover:border-tertiary-200-30',
+        tertiary:
+          'text-text-white border-gray-border bg-gray-card hover:bg-gray-border hover:border-text-muted',
         transparent: 'text-text-muted border-none bg-transparent',
       },
       size: {
