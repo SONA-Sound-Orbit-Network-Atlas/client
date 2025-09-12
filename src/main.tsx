739e27a3d38fdcb834cca5ba94aa63e4fb2fbc4c
Kang Ye Ram <kyj5730@gmail.com>
2025-09-12 15:14:00 +0900

    chore: 백엔드 API 연동을 위해 MSW 비활성화 하되 나중에 MSW를 다시 활성화 할 수 있도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-// import enableMocking from './mocks/anableMocking.ts';
+import enableMocking from './mocks/anableMocking.ts';
 
-// MSW 비활성화 - 실제 API 테스트를 위해
-// enableMocking().then(() => {
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-// });
+// MSW 조건부 활성화
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
