bc6ec340e3e347c224fb151f1149e56330628af8
Sori <chan8919@naver.com>
2025-09-15 09:41:41 +0900

    test: 패널 오너여부 테스트를 위해 무효화

--- src/components/panel/stellar/StellarIndex.tsx
+++ src/components/panel/stellar/StellarIndex.tsx
@@ -22,7 +22,9 @@ export default function StellarIndex() {
   // true 수정 모드 / false 관람 모드
   const { stellarStore } = useStellarStore();
   const { userStore } = useUserStore();
-  const isStellarOwner = stellarStore.userId === userStore.userId;
+  // const isStellarOwner = stellarStore.userId === userStore.userId;
+  //test용
+  const isStellarOwner = true;
   console.log('stellarStore.userId : ', stellarStore.userId);
   console.log('userStore.userId : ', userStore.userId);
   console.log('isStellarOwner : ', isStellarOwner);
