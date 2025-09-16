8c766c0ffe336c894c187fb50fdacc6634be92bf
Sori <chan8919@naver.com>
2025-09-17 04:28:20 +0900

    test: 씬 테스트 버튼 제거

--- src/pages/SpacePage.tsx
+++ src/pages/SpacePage.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react';
 import Scene from '../components/Scene';
 import Header from '@/components/layout/Header';
 import Sidebar from '@/components/layout/Sidebar/Sidebar';
-import TestButtons from '@/components/cosmos/TestButtons';
+// import TestButtons from '@/components/cosmos/TestButtons';
 
 export default function SpacePage() {
   const canvasStyle = {
@@ -19,7 +19,7 @@ export default function SpacePage() {
         <Sidebar />
         <div className="flex-1 flex flex-col min-h-0">
           <h1 className="text-white p-4">Space</h1>
-          <TestButtons />
+          {/* <TestButtons /> */}
           <div className="flex-1 min-h-0 relative">
             <Canvas style={canvasStyle} shadows>
               <Suspense fallback={null}>
